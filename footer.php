1ccfcc85cefc77e04c83f7367c57bf0abce7e93c
AINUL ALAM <104184992+Ainulalam@users.noreply.github.com>
2022-10-10 12:03:10 +0530

    Login And Logout page is ready

---
+++ footer.php
@@ -0,0 +1,11 @@
+<div id ="footer">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <span>© Copyright 2019 News | Powered by AINUL ALAM</span>
+            </div>
+        </div>
+    </div>
+</div>
+</body>
+</html>
